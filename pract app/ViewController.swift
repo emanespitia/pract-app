09229d2a65aade3883783ed841cd9f006dfa05ea
emanespitia <eespiti@jacksonville.edu>
2025-08-26 15:39:53 -0400

    Initial Commit

---
+++ pract app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pract app
+//
+//  Created by emanuel espitia on 8/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pract appTests/pract_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  pract_appTests.swift
+//  pract appTests
+//
+//  Created by emanuel espitia on 8/26/25.
+//
+
+import Testing
+@testable import pract_app
+
+struct pract_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
